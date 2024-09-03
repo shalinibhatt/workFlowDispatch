c002a80b08f7f11ef2aa759f7574bb42d027eb6f
shalinibhatt <shalinibhatt6263@gmail.com>
2024-09-03 20:31:11 +0530

    My first commit ---into the main
            *This addresses to main

---
+++ App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text>HELLO GUYS!! THIS IS MAIN BRANCH</Text>
+      <Text>HELLO GUYS!! THIS IS MAIN BRANCH</Text>
+      <Text>HELLO GUYS!! THIS IS MAIN BRANCH</Text>
+      <Text>HELLO GUYS!! THIS IS MAIN BRANCH</Text>
+      <Text>HELLO GUYS!! THIS IS MAIN BRANCH</Text>
+      <Text>HELLO GUYS!! THIS IS MAIN BRANCH</Text>
+      <Text>HELLO GUYS!! THIS IS MAIN BRANCH</Text>
+      <Text>HELLO GUYS!! THIS IS MAIN BRANCH</Text>
+      <Text>HELLO GUYS!! THIS IS MAIN BRANCH</Text>
+      <Text>HELLO GUYS!! THIS IS MAIN BRANCH</Text>
+      <Text>HELLO GUYS!! THIS IS MAIN BRANCH</Text>
+      <Text>HELLO GUYS!! THIS IS MAIN BRANCH</Text>
+      <Text>HELLO GUYS!! THIS IS MAIN BRANCH</Text>
+      <Text>HELLO GUYS!! THIS IS MAIN BRANCH</Text>
+      <Text>HELLO GUYS!! THIS IS MAIN BRANCH</Text>
+      <Text>HELLO GUYS!! THIS IS MAIN BRANCH</Text>
+      <Text>HELLO GUYS!! THIS IS MAIN BRANCH</Text>
+      <Text>HELLO GUYS!! THIS IS MAIN BRANCH</Text>
+      <Text>HELLO GUYS!! THIS IS MAIN BRANCH</Text>
+      <Text>HELLO GUYS!! THIS IS MAIN BRANCH</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;

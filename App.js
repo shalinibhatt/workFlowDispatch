72a439325cc16c59cff5b478b9ff0b2371fa79e7
shalinibhatt <shalinibhatt6263@gmail.com>
2024-09-12 11:25:09 +0530

    Upgrade to version 7.7.7

--- App.js
+++ App.js
@@ -4,11 +4,11 @@ import {SafeAreaView, Text} from 'react-native';
 const App = () => {
   return (
     <SafeAreaView>
+      <Text>HELLO Ghguyguggur</Text>
+      <Text>HELLO GUYS!! THIS IS kokokook1</Text>
       <Text>HELLO GUYS!! THIS IS TICKET-1</Text>
-      <Text>HELLO GUYS!! THIS IS TICKET-1</Text>
-      <Text>HELLO GUYS!! THIS IS TICKET-1</Text>
-      <Text>HELLO GUYS!! THIS IS TICKET-1</Text>
-      <Text>HELLO GUYS!! THIS IS TICKET-1</Text>
+      <Text>HELLO GUYS!! THIS IS shalini-1</Text>
+      <Text>HELLO GUYS!! THIS IS</Text>
       <Text>HELLO GUYS!! THIS IS TICKET-1</Text>
       <Text>HELLO GUYS!! THIS IS TICKET-1</Text>
       <Text>HELLO GUYS!! THIS IS TICKET-1</Text>
